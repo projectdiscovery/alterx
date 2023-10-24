54cdae47a3bbafce3c402fec37bbeec862ac0417
Doğan Can Bakır <dogancanbakir@protonmail.com>
2023-10-24 08:56:12 +0000

    minor

--- mutator.go
+++ mutator.go
@@ -156,6 +156,11 @@ func (m *Mutator) ExecuteWithWriter(Writer io.Writer) error {
 			// drain all dedupers when max-file size reached
 			continue
 		}
+
+		if strings.HasPrefix(value, "-") {
+			continue
+		}
+
 		outputData := []byte(value + "\n")
 		if len(outputData) > maxFileSize {
 			maxFileSize = 0
